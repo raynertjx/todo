e055b4e70d3240bf1517a095f6ba7b2d19ebf5dc
raynertjx <rayner742@gmail.com>
2022-04-01 22:35:36 +0800

    Added the add projects functionality with localStorage

--- main.js
+++ main.js
@@ -7,26 +7,90 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
 /***/ "./src/index.js":
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-/***/ (() => {
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("\n\n//# sourceURL=webpack://todo/./src/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _objects__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./objects */ \"./src/objects.js\");\n\n\nconst projectsContainer = document.querySelector('[data-projects]')\n\nconst addProjectForm = document.querySelector('[data-project-form]')\nconst projectInput = document.querySelector('[data-project-input]')\nconst addProjectButton = document.querySelector('[data-add-project-btn]')\n\nconst LOCAL_STORAGE_LIST_KEY = 'task.projects'\n\nlet projects = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || []\n\nfunction render() {\n  clearElement(projectsContainer)\n  projects.forEach(project => {\n    const li = document.createElement('li');\n    li.dataset.projectId = project.id\n    li.textContent = project.name\n    li.classList.add('indiv-project')\n    projectsContainer.appendChild(li)\n  })\n}\n\naddProjectForm.addEventListener(\"click\", (e) => {\n  e.preventDefault();\n  console.log('hi')\n  const projectName = projectInput.value\n  if (projectName == null || projectName === '') return\n  projectInput.value = null\n  const newProject = (0,_objects__WEBPACK_IMPORTED_MODULE_0__.createProject)(projectName)\n  projects.push(newProject)\n  localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(projects))\n  render()\n})\n\nfunction clearElement(element) {\n  while (element.firstChild) {\n    element.removeChild(element.firstChild)\n  }\n}\n\nrender()\n\n//# sourceURL=webpack://todo/./src/index.js?");
+
+/***/ }),
+
+/***/ "./src/objects.js":
+/*!************************!*\
+  !*** ./src/objects.js ***!
+  \************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"createProject\": () => (/* binding */ createProject),\n/* harmony export */   \"createTask\": () => (/* binding */ createTask)\n/* harmony export */ });\nfunction createTask(title, desc, date, priority) {\n  return {\n    title: title,\n    desc: desc,\n    date: date,\n    priority: priority\n  }\n}\n\nfunction createProject(name) {\n  return {\n    id: Date.now().toString(),\n    name: name,\n    tasks: []\n  }\n}\n\n\n\n//# sourceURL=webpack://todo/./src/objects.js?");
 
 /***/ })
 
 /******/ 	});
 /************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
 /******/ 	
 /******/ })()
 ;

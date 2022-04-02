7aec7b704120084b33f46511f221d8b0bcdf0c74
raynertjx <rayner742@gmail.com>
2022-04-02 18:24:05 +0800

    Added project attribute to Task object

--- src/objects.js
+++ src/objects.js
@@ -1,10 +1,11 @@
-function createTask(name, desc, date, priority) {
+function createTask(name, desc, date, priority, project) {
   return {
     id: Date.now().toString(),
     name: name,
     desc: desc,
     date: date,
     priority: priority,
+    project: project,
     complete: false
   }
 }

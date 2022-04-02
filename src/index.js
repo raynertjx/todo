ecbd0fc16f14dea8778432c20b9ca345b5d0a4a9
raynertjx <rayner742@gmail.com>
2022-04-02 12:02:45 +0800

    Added active project changing header and basic styling

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import { createTask, createProject } from './objects'
 
+const homeContainer = document.querySelector('[data-home]')
 const projectsContainer = document.querySelector('[data-projects]')
 
 const addProjectForm = document.querySelector('[data-project-form]')
 const projectInput = document.querySelector('[data-project-input]')
 const addProjectButton = document.querySelector('[data-add-project-btn]')
+const projectHeading = document.querySelector('[data-project-heading]')
 
-const LOCAL_STORAGE_LIST_KEY = 'task.projects'
+const LOCAL_STORAGE_PROJECT_KEY = 'task.projects'
+const LOCAL_STORAGE_SELECTED_PROJECT_KEY = 'task.selected.project'
 
-let projects = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || []
+let projects = JSON.parse(localStorage.getItem(LOCAL_STORAGE_PROJECT_KEY)) || []
+let selectedProjectID = localStorage.getItem(LOCAL_STORAGE_SELECTED_PROJECT_KEY)
 
 function render() {
   clearElement(projectsContainer)
@@ -18,8 +22,36 @@ function render() {
     li.textContent = project.name
     li.classList.add('indiv-project')
     projectsContainer.appendChild(li)
+    if (li.dataset.projectId === selectedProjectID) {
+      li.classList.add("project-active") 
+      projectHeading.textContent = li.textContent
+    }
   })
+  if (selectedProjectID == 'home') {
+    homeContainer.classList.add("project-active")
+    projectHeading.textContent = homeContainer.textContent
+  }
 }
+
+function save() {
+  localStorage.setItem(LOCAL_STORAGE_PROJECT_KEY, JSON.stringify(projects))
+  localStorage.setItem(LOCAL_STORAGE_SELECTED_PROJECT_KEY, selectedProjectID)
+}
+
+function saveAndRender() {
+  save()
+  render()
+} 
+
+homeContainer.addEventListener("click", (e) => {
+  selectedProjectID = e.target.dataset.projectId
+  saveAndRender()
+})
+
+projectsContainer.addEventListener("click", (e) => {
+  selectedProjectID = e.target.dataset.projectId
+  saveAndRender()
+})
 
 addProjectForm.addEventListener("click", (e) => {
   e.preventDefault();
@@ -29,11 +61,11 @@ addProjectForm.addEventListener("click", (e) => {
   projectInput.value = null
   const newProject = createProject(projectName)
   projects.push(newProject)
-  localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(projects))
-  render()
+  saveAndRender()
 })
 
 function clearElement(element) {
+  homeContainer.classList.remove("project-active")
   while (element.firstChild) {
     element.removeChild(element.firstChild)
   }
